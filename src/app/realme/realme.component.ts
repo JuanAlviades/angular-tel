61a89cae8b45d08ea2ad3628dee36917f5cab97b
JuanAlviades <jcamilo.01_@hotmail.com>
2024-02-20 19:53:00 -0500

    creación de los componentes faltantes

---
+++ src/app/realme/realme.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-realme',
+  standalone: true,
+  imports: [],
+  templateUrl: './realme.component.html',
+  styleUrl: './realme.component.scss'
+})
+export class RealmeComponent {
+
+}
