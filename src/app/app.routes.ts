6ae5d8fd216c7abe925b7de0866303028919fc90
JuanAlviades <jcamilo.01_@hotmai.com>
2024-02-27 21:02:27 -0500

    nuevos celulares, nuevas rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,10 @@ import path from 'path';
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { RealmeComponent } from './realme/realme.component';
+import { HuaweiComponent } from './huawei/huawei.component';
+import { SamsungComponent } from './samsung/samsung.component';
+import { MotorolaComponent } from './motorola/motorola.component';
+import { OppoComponent } from './oppo/oppo.component';
 
 export const routes: Routes = [
     {
@@ -14,6 +18,19 @@ export const routes: Routes = [
     },
     {
         path: 'realme', component: RealmeComponent,
+    },
+    {
+        path: 'huawei', component: HuaweiComponent,
+    },
+    {
+        path: 'samsung', component: SamsungComponent,
+    },
+    {
+        path: 'motorola', component: MotorolaComponent,
+    },
+    {
+        path: 'oppo', component: OppoComponent,
     }
+
     
 ];
